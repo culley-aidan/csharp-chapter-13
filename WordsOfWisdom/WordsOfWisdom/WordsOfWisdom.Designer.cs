b71b4911b904085043476ef6d88e71a60ac4cc92
culley-aidan <culley-aidan@users.noreply.github.com>
2019-12-16 08:49:18 -0600

    created lblQuoteOne

--- WordsOfWisdom/WordsOfWisdom/WordsOfWisdom.Designer.cs
+++ WordsOfWisdom/WordsOfWisdom/WordsOfWisdom.Designer.cs
@@ -28,20 +28,35 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.lblQuoteOne = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // Form1
+            // lblQuoteOne
+            // 
+            this.lblQuoteOne.AutoSize = true;
+            this.lblQuoteOne.Location = new System.Drawing.Point(13, 13);
+            this.lblQuoteOne.Name = "lblQuoteOne";
+            this.lblQuoteOne.Size = new System.Drawing.Size(271, 26);
+            this.lblQuoteOne.TabIndex = 0;
+            this.lblQuoteOne.Text = "War is peace. Freedom is slavery. Ignorance is strength.\r\n- George Orwell\r\n";
+            this.lblQuoteOne.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // WordsOfWisdom
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Name = "Form1";
+            this.Controls.Add(this.lblQuoteOne);
+            this.Name = "WordsOfWisdom";
             this.Text = "WordsOfWisdom";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblQuoteOne;
     }
 }
 
