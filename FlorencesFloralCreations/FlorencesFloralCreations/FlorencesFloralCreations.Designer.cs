eb3cce27f766059b4a396ed8cb48be7df700fd2f
culley-aidan <culley-aidan@users.noreply.github.com>
2019-12-16 09:30:02 -0600

    Update FlorencesFloralCreations.Designer.cs

--- FlorencesFloralCreations/FlorencesFloralCreations/FlorencesFloralCreations.Designer.cs
+++ FlorencesFloralCreations/FlorencesFloralCreations/FlorencesFloralCreations.Designer.cs
@@ -28,13 +28,37 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.lstOccasions = new System.Windows.Forms.ListBox();
+            this.lstFlowers = new System.Windows.Forms.ListBox();
             this.SuspendLayout();
+            // 
+            // lstOccasions
+            // 
+            this.lstOccasions.FormattingEnabled = true;
+            this.lstOccasions.Items.AddRange(new object[] {
+            "Funeral",
+            "Wedding",
+            "Prison"});
+            this.lstOccasions.Location = new System.Drawing.Point(13, 13);
+            this.lstOccasions.Name = "lstOccasions";
+            this.lstOccasions.Size = new System.Drawing.Size(120, 95);
+            this.lstOccasions.TabIndex = 0;
+            // 
+            // lstFlowers
+            // 
+            this.lstFlowers.FormattingEnabled = true;
+            this.lstFlowers.Location = new System.Drawing.Point(140, 13);
+            this.lstFlowers.Name = "lstFlowers";
+            this.lstFlowers.Size = new System.Drawing.Size(120, 95);
+            this.lstFlowers.TabIndex = 1;
             // 
             // FlorencesFloralCreations
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lstFlowers);
+            this.Controls.Add(this.lstOccasions);
             this.Name = "FlorencesFloralCreations";
             this.Text = "FlorencesFloralCreation";
             this.ResumeLayout(false);
@@ -42,6 +66,9 @@
         }
 
         #endregion
+
+        private System.Windows.Forms.ListBox lstOccasions;
+        private System.Windows.Forms.ListBox lstFlowers;
     }
 }
 
