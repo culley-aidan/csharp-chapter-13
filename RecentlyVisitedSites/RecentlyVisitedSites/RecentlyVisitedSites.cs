76444bb61a36bc24f037c0bb4547fd0f587ee6e4
culley-aidan <culley-aidan@users.noreply.github.com>
2019-12-16 09:24:27 -0600

    Update RecentlyVisitedSites.cs

--- RecentlyVisitedSites/RecentlyVisitedSites/RecentlyVisitedSites.cs
+++ RecentlyVisitedSites/RecentlyVisitedSites/RecentlyVisitedSites.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -16,5 +17,18 @@ namespace RecentlyVisitedSites
         {
             InitializeComponent();
         }
+
+        private void lnkOne_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+            => Process.Start(new Uri("https://www.google.com/").ToString());
+        private void lnkOne_MouseHover(object sender, EventArgs e)
+            => lblInfo.Text = "very epic search engine";
+        private void lnkTwo_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+            => Process.Start(new Uri("https://www.wikipedia.org/").ToString());
+        private void lnkTwo_MouseHover(object sender, EventArgs e)
+            => lblInfo.Text = "very epic wiki";
+        private void lnkThree_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+            => Process.Start(new Uri("https://www.microsoft.com/en-us/").ToString());
+        private void lnkThree_MouseHover(object sender, EventArgs e)
+            => lblInfo.Text = "very big company";
     }
 }
