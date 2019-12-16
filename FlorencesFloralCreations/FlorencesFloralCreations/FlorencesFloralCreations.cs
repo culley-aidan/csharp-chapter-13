e980d170b9ea633edec0321959d3ea41efa1b827
culley-aidan <culley-aidan@users.noreply.github.com>
2019-12-16 09:38:45 -0600

    Update FlorencesFloralCreations.cs

--- FlorencesFloralCreations/FlorencesFloralCreations/FlorencesFloralCreations.cs
+++ FlorencesFloralCreations/FlorencesFloralCreations/FlorencesFloralCreations.cs
@@ -16,5 +16,47 @@ namespace FlorencesFloralCreations
         {
             InitializeComponent();
         }
+
+        private void lstOccasions_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            lstFlowers.Items.Clear();
+            switch (lstOccasions.SelectedIndex)
+            {
+                case -1:
+                    lstFlowers.Visible = false;
+                    break;
+                case 0:
+                    lstFlowers.Items.Add("Red Roses");
+                    lstFlowers.Items.Add("Pink snapdragons");
+                    lstFlowers.Visible = true;
+                    break;
+                case 1:
+                    lstFlowers.Items.Add("Poppy");
+                    lstFlowers.Items.Add("Daisy");
+                    lstFlowers.Visible = true;
+                    break;
+                case 2:
+                    lstFlowers.Items.Add("Blanketflower");
+                    lstFlowers.Items.Add("Iris");
+                    lstFlowers.Visible = true;
+                    break;
+            }
+        }
+
+        private void lstFlowers_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            lblCongrats.Visible = false;
+            lblCongrats.Text = "Congrats!";
+            switch(lstFlowers.SelectedIndex)
+            {
+                case -1:
+                    lblCongrats.Visible = false;
+                    break;
+                case 0:
+                case 1:
+                    lblCongrats.Visible = true;
+                    break;
+            }
+        }
     }
 }
