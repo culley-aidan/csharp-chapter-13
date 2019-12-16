cd382fd4fa780d58ac861739f8addbb721a6eb30
culley-aidan <culley-aidan@users.noreply.github.com>
2019-12-16 10:06:38 -0600

    Update GuessANumber.cs

--- GuessANumber/GuessANumber/GuessANumber.cs
+++ GuessANumber/GuessANumber/GuessANumber.cs
@@ -1,20 +1,27 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace GuessANumber
 {
     public partial class GuessANumber : Form
     {
+        private int randomNumber = new Random().Next(0, 6);
         public GuessANumber()
         {
             InitializeComponent();
+        }
+
+        private void rad_CheckedChanged(object sender, EventArgs e)
+        {
+            RadioButton[] rads = grpRad.Controls.OfType<RadioButton>().ToArray();
+            RadioButton correctRad = rads[randomNumber];
+            RadioButton selectedRad = rads.FirstOrDefault(x => x.Checked);
+            if (selectedRad == correctRad) {
+                lblHint.Text = "Correct!";
+            } else {
+                lblHint.Text = "Try Again!";
+            }
         }
     }
 }
